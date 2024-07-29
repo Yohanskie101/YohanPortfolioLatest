987d63a8615e2e1b291d94fd1f0da25af9499126
Yohanskie101 <125958519+Yohanskie101@users.noreply.github.com>
2024-07-29 18:42:22 +0800

    Modals Added

--- src/components/Projectfirst.jsx
+++ src/components/Projectfirst.jsx
@@ -18,7 +18,11 @@ function Projectfirst() {
       </div>
 
       <div className="Project-img-cont w-full mb-5 h-1/2">
-        <img className="w-full h-full" src={FirstProjectImg} />
+        <img
+          className="w-full h-full"
+          src={FirstProjectImg}
+          onClick={() => document.getElementById("my_modal_1").showModal()}
+        />
       </div>
 
       <div className="relative">
@@ -40,6 +44,19 @@ function Projectfirst() {
           </div>
         </div>
       </div>
+
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">ITrak</p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </>
   );
 }
--- src/components/Projectsecond.jsx
+++ src/components/Projectsecond.jsx
@@ -18,7 +18,11 @@ function Projectsecond() {
       </div>
 
       <div className="Project-img-cont w-full mb-5 h-1/2">
-        <img className="w-full h-full" src={SecondProjectImg} />
+        <img
+          className="w-full h-full"
+          src={SecondProjectImg}
+          onClick={() => document.getElementById("my_modal_1").showModal()}
+        />
       </div>
 
       <div className="relative">
@@ -40,6 +44,19 @@ function Projectsecond() {
           </div>
         </div>
       </div>
+
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Javah Catering Service</p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </>
   );
 }
